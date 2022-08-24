25a31a4db4bd554ef339b5dda04744cfc7eeb3da
csenjoy <70559266@qq.com>
2022-08-24 15:53:30 +0800

    first commit for ActivityTest Helloworld and FirstActivity

---
+++ Helloworld/app/src/main/java/com/csenjoy/helloworld/MainActivity.java
@@ -0,0 +1,52 @@
+package com.csenjoy.helloworld;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = findViewById(R.id.button1);
+        if (btn1 != null) {
+            btn1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item: {
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.remove_item: {
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            default:
+                break;
+        }
+        return true;
+    }
+
+}

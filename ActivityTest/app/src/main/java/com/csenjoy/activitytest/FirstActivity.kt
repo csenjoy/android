a1fbf7fe88aa1d2956cb39fede70e72699c8587b
csenjoy <70559266@qq.com>
2022-08-24 16:02:18 +0800

    commit activitytest project

---
+++ ActivityTest/app/src/main/java/com/csenjoy/activitytest/FirstActivity.kt
@@ -0,0 +1,35 @@
+package com.csenjoy.activitytest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.first_layout.*
+
+class FirstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.first_layout)
+        //Use Toast in main activity
+        //button1?.setOnClickListener {
+        //    Toast.makeText(this, "You clicked Button 1", Toast.LENGTH_SHORT).show()
+        //}
+        button1.setOnClickListener {
+           Toast.makeText(this, "You clicked Button 1", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true;
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show()
+        }
+        return  true;
+    }
+}
